de2690d00b016b98b1b1c62d6f41cf000fda9c00
ddening <44156662+ddening@users.noreply.github.com>
2023-04-14 23:24:53 +0200

    Basic Interrupt Routine

--- switches.c
+++ switches.c
@@ -1,39 +1,67 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
 
 #include "switches.h"
 #include "uart.h"
+#include "utils.h"
 
 typedef void (*fn_switch)( void );
 
+static fn_switch fn_switch_00 = NULL;
 static fn_switch fn_switch_01 = NULL;
 static fn_switch fn_switch_02 = NULL;
-static fn_switch fn_switch_03 = NULL;
 
-void switch_init( void ) {
-    
-    // Setup Pin Change Interrupts ...
-    
-    fn_switch fn_switch_01 = NULL;
-    fn_switch fn_switch_02 = NULL;
-    fn_switch fn_switch_03 = NULL;
+static void _cb_sw00 ( void ) {
+    uart_put("SWITCH 00");
 }
 
-ISR(PCINT0_vect) {
+static void _cb_sw01 ( void ) {
+    uart_put("SWITCH 01");
+}
+
+static void _cb_sw02 ( void ) {
+    uart_put("SWITCH 02");
+}
+
+void switch_init( void ) {
+   
+    fn_switch_00 = &_cb_sw00;
+    fn_switch_01 = &_cb_sw01;
+    fn_switch_02 = &_cb_sw02;
+    
+    /* Configure port/pins as OUTPUT/HIGH on default. Pulling LOW triggers an interrupt. */   
+    EXT_INT_DDR  = 0x00; // Set all ports as INPUT
+    EXT_INT_PORT = (1 << SWITCH00) | (1 << SWITCH01) | (1 << SWITCH00); // Pullup (HIGH)
+    
+    /* Trigger on falling edge */
+    EICRA |= (1 << ISC01) | (1 << ISC00);
+    EICRA |= (1 << ISC11) | (1 << ISC10);
+    EICRA |= (1 << ISC21) | (1 << ISC20);
+    
+    /* Activate External Interrupts */
+    EIMSK |= (1 << INT0) | (1 << INT1) | (1 << INT2);
+  
+    sei();
+    
+    uart_put("Switch Init");
+}
+
+ISR(INT0_vect) {
+    if (fn_switch_00 != NULL) {
+        fn_switch_00();
+    }
+}
+
+ISR(INT1_vect) {
     if (fn_switch_01 != NULL) {
         fn_switch_01();
     }
 }
 
-ISR(PCINT1_vect) {
+ISR(INT2_vect) {
     if (fn_switch_02 != NULL) {
         fn_switch_02();
-    }
-}
-
-ISR(PCINT2_vect) {
-    if (fn_switch_03 != NULL) {
-        fn_switch_03();
     }
 }
--- switches.h
+++ switches.h
@@ -1,6 +1,13 @@
 #ifndef SWITCHES_H_
 #define SWITCHES_H_
 
+#define EXT_INT_PORT PORTC
+#define EXT_INT_DDR DDRC
+
+#define SWITCH00 0x00
+#define SWITCH01 0x01
+#define SWITCH02 0x02
+
 void switch_init( void );
 
 #endif /* SWITCHES_H_ */
