872acc4a1184cb2570780b3e81a4d56bb392f151
ddening <44156662+ddening@users.noreply.github.com>
2023-04-15 14:58:39 +0200

    Added proper ISR and connected functions with switches.

--- switches.c
+++ switches.c
@@ -4,64 +4,84 @@
 #include <avr/io.h>
 
 #include "switches.h"
+#include "w204.h"
 #include "uart.h"
-#include "utils.h"
 
 typedef void (*fn_switch)( void );
 
 static fn_switch fn_switch_00 = NULL;
 static fn_switch fn_switch_01 = NULL;
 static fn_switch fn_switch_02 = NULL;
-
-static void _cb_sw00 ( void ) {
-    uart_put("SWITCH 00");
-}
-
-static void _cb_sw01 ( void ) {
-    uart_put("SWITCH 01");
-}
-
-static void _cb_sw02 ( void ) {
-    uart_put("SWITCH 02");
-}
+static fn_switch fn_switch_03 = NULL;
 
 void switch_init( void ) {
    
-    fn_switch_00 = &_cb_sw00;
-    fn_switch_01 = &_cb_sw01;
-    fn_switch_02 = &_cb_sw02;
+    fn_switch_00 = &w204_shift_cursor_left;
+    fn_switch_01 = &w204_shift_cursor_right;
+    fn_switch_02 = &w204_shift_display_left;
+    fn_switch_03 = &w204_shift_display_right;
     
-    /* Configure port/pins as OUTPUT/HIGH on default. Pulling LOW triggers an interrupt. */   
-    EXT_INT_DDR  = 0x00; // Set all ports as INPUT
-    EXT_INT_PORT = (1 << SWITCH00) | (1 << SWITCH01) | (1 << SWITCH00); // Pullup (HIGH)
+    // EXT_INT_DDR  = 0x00; // Set all ports as INPUT
     
-    /* Trigger on falling edge */
-    EICRA |= (1 << ISC01) | (1 << ISC00);
-    EICRA |= (1 << ISC11) | (1 << ISC10);
-    EICRA |= (1 << ISC21) | (1 << ISC20);
+    #ifdef SWITCH00
+    EXT_INT_DDR &= ~(1 << SWITCH00);       // Configure port as INPUT
+    EXT_INT_PORT |= (1 << SWITCH00);       // Pullup (HIGH)
+    EICRA |= (1 << ISC01) | (1 << ISC00);  // Trigger on falling edge
+    EIMSK |= (1 << INT0);                  // Activate External Interrupt
+    #endif
     
-    /* Activate External Interrupts */
-    EIMSK |= (1 << INT0) | (1 << INT1) | (1 << INT2);
+    #ifdef SWITCH01
+    EXT_INT_DDR &= ~(1 << SWITCH01);       // Configure port as INPUT
+    EXT_INT_PORT |= (1 << SWITCH01);       // Pullup (HIGH)
+    EICRA |= (1 << ISC11) | (1 << ISC10);  // Trigger on falling edge
+    EIMSK |= (1 << INT1);                  // Activate External Interrupt
+    #endif
+    
+    #ifdef SWITCH02
+    EXT_INT_DDR &= ~(1 << SWITCH02);       // Configure port as INPUT
+    EXT_INT_PORT |= (1 << SWITCH02);      // Pullup (HIGH)
+    EICRA |= (1 << ISC21) | (1 << ISC20); // Trigger on falling edge
+    EIMSK |= (1 << INT2);                 // Activate External Interrupt
+    #endif
+    
+    #ifdef SWITCH03
+    EXT_INT_DDR &= ~(1 << SWITCH03);       // Configure port as INPUT
+    EXT_INT_PORT |= (1 << SWITCH03);      // Pullup (HIGH)
+    EICRA |= (1 << ISC31) | (1 << ISC30); // Trigger on falling edge
+    EIMSK |= (1 << INT3);                 // Activate External Interrupt
+    #endif 
   
     sei();
-    
-    uart_put("Switch Init");
 }
 
+#ifdef SWITCH00
 ISR(INT0_vect) {
     if (fn_switch_00 != NULL) {
         fn_switch_00();
     }
 }
+#endif
 
+#ifdef SWITCH01
 ISR(INT1_vect) {
     if (fn_switch_01 != NULL) {
         fn_switch_01();
     }
 }
+#endif
 
+#ifdef SWITCH02
 ISR(INT2_vect) {
     if (fn_switch_02 != NULL) {
         fn_switch_02();
     }
 }
+#endif
+
+#ifdef SWITCH03
+ISR(INT3_vect) {
+    if (fn_switch_03 != NULL) {
+        fn_switch_03();
+    }
+}
+#endif
--- switches.h
+++ switches.h
@@ -1,12 +1,26 @@
 #ifndef SWITCHES_H_
 #define SWITCHES_H_
 
-#define EXT_INT_PORT PORTC
-#define EXT_INT_DDR DDRC
+#include <avr/io.h>
 
-#define SWITCH00 0x00
-#define SWITCH01 0x01
-#define SWITCH02 0x02
+/* SPI Port Declaration */
+#if defined(__AVR_ATmega1284P__)
+#   define EXT_INT_PORT PORTD
+#   define EXT_INT_DDR DDRD
+#   define SWITCH00 0x00
+#   define SWITCH01 0x01
+# elif defined(__AVR_ATmega2560__)
+#   define EXT_INT_PORT PORTD
+#   define EXT_INT_DDR DDRD
+#   define SWITCH00 0x00
+#   define SWITCH01 0x01
+#   define SWITCH02 0x02
+#   define SWITCH03 0x03
+#else
+#  if !defined(__COMPILING_AVR_LIBC__)
+#    warning "Microcontroller not defined."
+#  endif
+#endif
 
 void switch_init( void );
 
